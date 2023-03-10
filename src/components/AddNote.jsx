f1200fdfb43deabb21633c713747fa164dc4e2c8
Carlos-Gabriel-Rojas <carlosgabrielrojas.dev@gmail.com>
2023-03-10 13:07:40 -0300

    structure refactor

---
+++ src/components/AddNote.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const AddNote = ({ handleAddNote }) => {
+  const [noteText, setNoteText] = useState("");
+
+  const characterLimit = 200;
+
+  const handleChange = (e) => {
+    if (e.target.value.length <= characterLimit) {
+      setNoteText(e.target.value);
+    }
+  };
+
+  const handleSaveClick = () => {
+    if (noteText.trim().length > 0) {
+      handleAddNote(noteText);
+      setNoteText("");
+    }
+  };
+
+  return (
+    <div className="new-note">
+      <textarea
+        rows="8"
+        cols="10"
+        placeholder="Type to add a note..."
+        value={noteText}
+        onChange={handleChange}
+      ></textarea>
+      <div className="note-footer">
+        <small>{characterLimit - noteText.length} Remaining</small>
+        <button className="save" onClick={handleSaveClick}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddNote;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="header">
+    <h1>Notes4U</h1>
+  </div>
+);
+
+export default Header;
